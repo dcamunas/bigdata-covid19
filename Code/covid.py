7dd2cc86fe7490846864d54c64480b4254db79f4
Mario Pérez <48880789+mapecode@users.noreply.github.com>
2020-05-05 19:51:41 +0200

    Delete covid.py

--- Code/covid.py
+++
@@ -1,6 +0,0 @@
-from mongo_client import CovidClient
-
-if __name__ == '__main__':
-    client = CovidClient('mongodb://192.168.0.110:27017/', 'covid', 'Worldwide')
-
-    print(client.get_data_date_country('Spain', '07/04/2020'))
